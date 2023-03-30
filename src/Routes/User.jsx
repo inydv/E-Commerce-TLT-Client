085b35299666162dc831020d4ef8fa3c91ab383b
inydv <i_nydv@hotmail.com>
2023-03-30 20:57:32 +0530

    About

--- src/Routes/User.jsx
+++ src/Routes/User.jsx
@@ -1,10 +1,11 @@
-import { Home, Auth } from "../Pages/index";
+import { Home, Auth, About } from "../Pages/index";
 
 export default function User() {
   return (
     <div className="min-h-[calc(100vh-350px)] bg-black">
       {/* <Home /> */}
-      <Auth />
+      {/* <Auth /> */}
+      <About />
     </div>
   );
 }
