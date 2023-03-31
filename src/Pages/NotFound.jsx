166a85a328d5bb5c092e7efdc6132e61138a53e9
inydv <i_nydv@hotmail.com>
2023-03-31 18:25:34 +0530

    NotFound

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,5 +1,16 @@
 export default function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <>
+      <div className="bg-white bg-[url('src/Assets/notFound.gif')] bg-no-repeat bg-center h-[420px] w-full"></div>
+      <div className="grid justify-center p-5">
+        <h1 className="text-center text-xl font-semibold">
+          Look Like You're lost
+        </h1>
+        <h3 className="text-center text-xl font-semibold pt-2 pb-4">
+          The Page You're Looking For Not Available!
+        </h3>
+        <button className="bg-button py-2 px-5 text-lg font-semibold">Go To Home</button>
+      </div>
+    </>
+  );
 }
--- src/Routes/User.jsx
+++ src/Routes/User.jsx
@@ -1,13 +1,22 @@
-import { Home, Auth, About, Contact, Detail } from "../Pages/index";
+import { Home, Auth, About, Contact, Detail, NotFound, ConfirmOrder, MyAccount, MyOrders, OrderDetails, OrderSuccess, Payment, Shipping, Shop } from "../Pages/index";
 
 export default function User() {
   return (
     <div className="min-h-[calc(100vh-350px)] bg-black">
       {/* <About /> */}
-      <Auth />
+      {/* <Auth /> */}
       {/* <Home /> */}
       {/* <Contact /> */}
-      {/* <Detail /> */}
+      {/* <NotFound /> */}
+      <Detail />
+      {/* <ConfirmOrder /> */}
+      {/* <MyAccount /> */}
+      {/* <MyOrders /> */}
+      {/* <OrderDetails /> */}
+      {/* <OrderSuccess /> */}
+      {/* <Payment /> */}
+      {/* <Shipping /> */}
+      {/* <Shop /> */}
     </div>
   );
 }
