f4dd41356fd0b379c209789da8825d142e941306
inydv <i_nydv@hotmail.com>
2024-01-13 23:06:29 +0530

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    workbox: {
+      clientsClaim: true,
+      skipWaiting: true,
+    },
+    closeBundle: async () => {
+      try {
+        // Your custom code for the closeBundle hook
+        console.log('closeBundle hook executed successfully');
+      } catch (error) {
+        console.error('Error in closeBundle hook:', error);
+      }
+    },
+    devOptions: {
+      enabled: true,
+    },
+  })],
 })
